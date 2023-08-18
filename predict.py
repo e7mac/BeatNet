29773f43f45ffb6e963123632050a39c529ecf39
Mayank Sanganeria <mayank.ot@gmail.com>
2023-08-18 11:45:33 -0700

    add predict function

--- predict.py
+++ predict.py
@@ -3,20 +3,19 @@
 
 from cog import BasePredictor, Input, Path
 
+from BeatNet.BeatNet import BeatNet
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         # self.model = torch.load("./weights.pth")
+        self.estimator = BeatNet(1, mode='offline', inference_model='DBN', plot=[], thread=False)
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        scale: float = Input(
-            description="Factor to scale image by", ge=0, le=10, default=1.5
-        ),
-    ) -> Path:
+        audio: Path = Input(description="Path to the input music. Supports mp3 and wav format."),
+    ) -> str:
         """Run a single prediction on the model"""
-        # processed_input = preprocess(image)
-        # output = self.model(processed_image, scale)
-        # return postprocess(output)
+        estimator = BeatNet(1, mode='offline', inference_model='DBN', plot=[], thread=False)
+        beats = self.estimator.process(audio).tolist()
+        return str(beats)
